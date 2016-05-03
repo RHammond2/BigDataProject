e9a3fd623df11ef445e17c84d8d3165bd79eb4de
Rob Hammond <RHammond2@users.noreply.github.com>
2016-05-03 09:12:06 -0400

    reducer

--- reduce_d.py
+++ reduce_d.py
@@ -3,60 +3,89 @@
 #NOTE: Length 23 = weather
 
 import sys
+import re
+import string
+
 
 #key = None
 #weather = []
 #trips = []
 combined={}
 #times =[]
-
-lines = sys.stdin
 data = []
 
 def mapfloat(x):
 	try:
 		x = float(x)
 	except ValueError:
-		x = x
+		return x	# x = x
 	return x
 
-for line in lines:
-	line = line.strip().split("\t")
+for line in sys.stdin:
+
+	line = line.strip().split("\t",1)
 
 	k = line[0]
 	v = line[1]
+	#print k
 
-	if len(v.strip().split(',')) == 23:
+	v=v.strip().split(',')
+
+	if len(v) == 23:
 		""" WEATHER """
-		combined[k]=[v,0,0,0,0,0,0,0,0,0,0]
+		#v_string = '%s' %(v)
+		#v_string = v_string.translate(string.maketrans('', ''), '[')
+		#v_string = v_string.translate(string.maketrans('', ''), ']')
+		combined[k]=[v,None]#combined[k]=[v,0,0,0,0,0,0,0,0,0,0]
 	else:
 		data.append(line)
+	#print len()
+
 
 for line in data:
 	
+
 	#line = line.strip().split("\t",1)
 
 	k = line[0]
 	v = line[1]
-	v = v.strip().split(',')
 
-	if len(v) != 23:
-		""" TRIPS """
-		if (combined[k])[1] == 0:
-			v = map(mapfloat,v)
-			for i,val in zip(range(1,12),v):
-				(combined[k])[i] = val
-			#(combined[k])[1] = v.strip().split(',')
-		else:
+	v = map(mapfloat,v.strip().split(','))
 
-			trip_data = v
-			trip_data = map(mapfloat,trip_data)
+	#if len(v.strip().split(',')) != 23:
+	""" TRIPS """
+	if (combined[k])[1] == None:
+		(combined[k])[1] = v
+		#for i,val in zip(range(1,11),v):
+				#(combined[k])[i] = val
+	else:
 
-			for i in range(1,10):
-				(combined[k])[i] += trip_data[i]
+		#trip_data = v.strip().split(',')
+		#trip_data = map(mapfloat,trip_data)
+		trip_data = v
+
+		for i in range(10):
+			((combined[k])[1])[i] += float(trip_data[i])
 
 
+	#print k
+print ("year","month","day","hour","minute","USAF","WBAN","direction","speed","gust","cloud_ceiling","sky_cover","low_cloud","middle_cloud","high_cloud","visibility","temp","dewpoint","sea_level_pressure","altimeter","station_pressure","max_temp","min_temp","PCP01","PCP06","PCP24","PCPXX","snow_depth","passenger_count","trip_distance","fare_amount","extra","mta_tax","tip_amount","tolls_amount","improvement_surcharge","total_amount","Count")
 for key in combined:
-	print key, combined[key]
+
+	final = '%s,%s,%s' %(key, combined[key][0],combined[key][1])
+	try:
+		final = final.translate(string.maketrans('', ''), '[')
+	except:
+		dummy = 0
+	try:
+		final = re.sub(']', '',final)
+	except:
+		dummy = 3
+
+	print final
+
+#print "year,month,day,hour,minute,weather,agg_trips"
+#for line in data:
+	#print line
 
 
